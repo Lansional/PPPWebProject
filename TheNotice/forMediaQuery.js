5c261e1e516b3cdd7a0abfb107def80590dbbd5a
Lansional <cbg1720@gmail.com>
2022-11-21 23:19:16 +0900

    add animation at right

--- TheNotice/forMediaQuery.js
+++ TheNotice/forMediaQuery.js
@@ -1,9 +1,9 @@
-const mediaQuery = window.matchMedia('(max-width: 1024px)');
+// const mediaQuery = window.matchMedia('(max-width: 1024px)');
 
-var text = document.getElementById('text');
+// var text = document.getElementById('text');
 
-if (mediaQuery.matches) {
-    text.innerText = '≡';
-} else {
-    text.innerText = '회원가입';
-}
+// if (mediaQuery.matches) {
+//     text.innerText = '≡';
+// } else {
+//     text.innerText = '회원가입';
+// }
