c2e3f9736625f11b02eb9d5e6fdd31f793e98927
Lansional <cbg1720@gmail.com>
2022-12-01 12:24:40 +0900

    temp commit

--- Login/login.php
+++ Login/login.php
@@ -1,4 +1,7 @@
 <?php
+$db_conn = mysqli_connect("localhost", "s2217", "2217", "s2217_db") or die;
 
 
+
+mysqli_close($db_conn);
 ?>
--- Login/register.php
+++ Login/register.php
@@ -1,17 +1,23 @@
 <?php
-$db_conn = mysqli_connect("localhost", "s2217", "2217", "s2217_db") or die;
+$db_conn = mysqli_connect("localhost", "s2217", "2217", "s2217_db") or die('연결실패');
 
 $account = $_POST['id'];
 $passwd = $_POST['passwd'];
 
 $forTable = "clickgames";
-$result = mysqli_query("INSERT INTO $forTable VALUES ($id, $passwd, 0)");
+$result = mysqli_query($db_conn, "INSERT INTO $forTable VALUES ('$account', '$passwd', 0)");
 
-if ($result)
-    echo "<script>alert('성공');</script>";
-else
-    echo "<script>alert('실패');</script>";
+echo $account."<br>";
+echo $passwd."<br>";
+
+if ($result) {
+    // echo "<script>alert('성공');</script>";
+    echo "성공";
+} else {
+    // echo "<script>alert('실패');</script>";
+    echo "실패";
+}
 
 mysqli_close($db_conn);
-echo("<script>location.href='login.html';</script>");
+// echo("<script>location.href='login.html';</script>");
 ?>
