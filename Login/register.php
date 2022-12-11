c44b5db728c21dadd8e53bf671a18c4d44d5a945
Lansional <cbg1720@gmail.com>
2022-12-11 07:44:00 +0000

    reset login page

--- Login/register.php
+++ Login/register.php
@@ -13,11 +13,6 @@ include "../theServer.inc";
 $result = mysqli_query($db_conn, "INSERT INTO $forTable VALUES ('$account', '$passwd', 0)");
 
 if ($result) {
-    session_start();
-
-    $_SESSION['is_login'] = true;
-    $_SESSION['id'] = $account;
-
     echo "<script>alert('가입완료');</script>";
     echo "<script>location.href='login.html';</script>";
 } else {
