2e819b6dd0530907e84a573879d870b1bbb350a2
jwipo <taeyun7527@naver.com>
2022-12-11 09:18:35 +0000

    main leader ver.2

---
+++ Main/all-leader-board.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <link href="all-leader-board.css" rel="stylesheet"> 
+</head>
+<body   >
+<a href="leader-reset.html" target="leaderboard" style=" text-decoration: none;">
+    <div class="close">
+        <p class="text1">⮈ 창 닫기</p>
+    </div>
+    </a>
+    <div class="leader">
+    
+        <h1 class="text">전체 순위</h1>
+    </div>
+    <div class="all-leader">
+        <div class="emogi">  
+        <p class="main-ranking1">🥇</p>
+        </div>
+         <p class="name"> 이름</p>
+        <p class="score">0</p>
+    </div>
+    
+    <div class="all-leader2">
+        <div class="emogi"> 
+         <p class="main-ranking1">🥈</p>
+        </div>
+        <p class="name"> 이름</p>
+        <p class="score">0</p>
+    </div>
+</body>
+</html>
--- Main/main.php
+++ Main/main.php
@@ -6,41 +6,67 @@
     ?>
     <link href="main.css" rel="stylesheet">
     <script>
-        function openNav() 
-        {
-            document.getElementById("mySidenav").style.height = "160px";
-        }
-        function closeNav()
-        {
-            document.getElementById("mySidenav").style.height = "0";
-        }
+        // int i=1;
+        // function openNav() 
+        // {
+        //     i++;
+        //     if(i=2)
+        //     {
+        //     function openNav()
+        //     {
+        //     document.getElementById("mySidenav").style.height = "0";
+        //     }
+        //     i/2=i;
+        //     }
+        //     document.getElementById("mySidenav").style.height = "160px";
+        // }
+        
+
+//         var hiddenBtn = document.getElementById('hiddenBtn');
+
+// hiddenBtn.addEventListener('click', function openNav() {
+//     var mySidenav = document.getElementById('mySidenav');
+
+//     if (mySidenav.style.height == '160px') {
+//         mySidenav.style.height = '0px';
+//         mySidenav.style.visibility = 'hidden';
+//     } else {
+//         mySidenav.style.height = '160px';
+//         mySidenav.style.visibility = 'visible';
+//     }
+// });
         </script>
 </head>
 <body>
+    
     <iframe class="all-leader" name="leaderboard" ></iframe>
     
-    <a href="../TheNotice/theNotice.php" style="text-decoration: none; color: black;"><div class="menu1"><h3 style="margin-top: -0.1px; margin-left: 7px">➀</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">공지사항</h1></div></a>
+    <a href="../TheNotice/theNotice.php" style="text-decoration: none; color: black;"><div class="menu1"><h3 style="margin-top: -0.1px; margin-left: 7px">①</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">공지사항</h1></div></a>
+  
     <?php
     session_start();
 
     if (!$_SESSION['is_login']) {
     ?>
-        <a href="../Login/login.html" style="text-decoration: none; color: black;"><div class="menu2"><h3 style="margin-top: -0.1px; margin-left: 7px">➁</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">로그인</h1></div></a>
+        <a href="../Login/login.html" style="text-decoration: none; color: black;"><div class="menu2"><h3 style="margin-top: -0.1px; margin-left: 7px">②</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">로그인</h1></div></a>
         <?php
     } else {
         ?>
-        <a href="../Login/logout.php" style="text-decoration: none; color: black;"><div class="menu2"><h3 style="margin-top: -0.1px; margin-left: 7px">➁</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">로그아웃</h1></div></a>
+        <a href="../Login/logout.php"  style="text-decoration: none; color: black;"><div class="menu2"><h3 style="margin-top: -0.1px; margin-left: 7px">②</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">로그아웃</h1></div></a>
         <?php
     }
     ?>
-        
-        <a href="tel:https://popcat.click/" style="text-decoration: none; color: black;"><div class="menu3"><h3 style="margin-top: -0.1px; margin-left: 7px">③</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">링크 공유</h1></div></a>
+    <a href="tel:https://popcat.click/" style="text-decoration: none; color: black;"><div class="menu3"><h3 style="margin-top: -0.1px; margin-left: 7px">③</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">링크 공유</h1></div></a>
+    <a href="tel:https://popcat.click/" style="text-decoration: none; color: black;"><div class="menu4"><h3 style="margin-top: -0.1px; margin-left: 7px">④</h3><h3 style="text-align: center; margin-left: 10px; margin-top: -40px;">이용약관</h1></div></a>
+
+
+
         <div id="mySidenav" class="main-top-bar">
             <div  class="main-under-benner1">
                 <div class="main-under-benner2">
                     <iframe class="ads" src="놇두ads.html"></iframe>
                 </div>
-                <a href="#" target="">
+                <a href="all-leader-board.php" target="leaderboard">
                     <div style="float: right; margin: -6px 30px; height: 0;">
                         <h1 style="color: black;">
                             ≡
@@ -99,9 +125,9 @@
             </div>    
         </div>
     </div>
-
-    <a href="javascript:void(0)" onclick="openNav()" style="text-decoration: none;"> 
-        <div class="main-top-button">
+    
+    <a href="javascript:void(0)" onclick="openNav()" style="text-decoration: none;">
+        <div class="main-top-button" id="hiddenBtn">
             <h2 style="text-align: center; margin-top: -6px; color: black; height: 0;">
                 =
             </h2> 
@@ -130,9 +156,6 @@
     <?php
     }
     ?>
-        <!-- <p align="middle">
-            <iframe class="touch-score" src="plus.php" name="score"></iframe>
-        </p> -->
         <div href="plus.php">
             <div class="nohomrim"></div>
         </div>
