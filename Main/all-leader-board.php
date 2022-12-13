7a66d832c8c9d8a8890f5d9e4f23e77c50b369d8
Lansional <cbg1720@gmail.com>
2022-12-13 19:07:27 +0900

    add score board

--- Main/all-leader-board.php
+++ Main/all-leader-board.php
@@ -1,15 +1,15 @@
 <html>
 <head>
+    <?php
+    include "../theServer.inc";
+    $count = 4;
+    $result = mysqli_query($db_conn, "SELECT * FROM $forTable ORDER BY count DESC");
+    $row = mysqli_fetch_array($result);
+    ?>
     <link href="all-leader-board.css" rel="stylesheet">
     <meta http-equiv="refresh" content="5">
 </head>
 <body>
-    <?php
-    include "../theServer.inc";
-
-    $result = mysqli_query($db_conn, "SELECT * FROM $forTable ORDER BY count DESC");
-    $row = mysqli_fetch_array($result);
-    ?>
     <a href="leader-reset.html" target="leaderboard" style=" text-decoration: none;">
     <div class="close">
         <p class="text1">⮈ 창 닫기</p>
@@ -37,16 +37,27 @@
         <p class="score"><?=$row['count']?></p>
     </div>
     <?php
+    $row = mysqli_fetch_array($result);
+    ?>
+    <div class="all-leader">
+        <div class="emogi">  
+        <p class="main-ranking1">🥉</p>
+        </div>
+         <p class="name"><?=$row['account']?></p>
+        <p class="score"><?=$row['count']?></p>
+    </div>
+    <?php
     while ($row = mysqli_fetch_array($result)) {
     ?>
     <div class="all-leader">
-        <!-- <div class="emogi">   -->
-        <!-- <p class="main-ranking1">🥈</p> -->
-        <!-- </div> -->
+        <div class="emogi">  
+        <p class="main-ranking1"><?=$count?></p>
+        </div>
         <p class="name"><?=$row['account']?></p>
         <p class="score"><?=$row['count']?></p>
     </div>
     <?php
+    $count++;
     }
     mysqli_close($db_conn);
     ?>
